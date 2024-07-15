d03e25290b5a7ceefd8565211beff4bcc5f84bb1
akda007 <akdagmr007s@gmail.com>
2024-07-15 15:32:55 -0300

    Game is now playable, added map clear functionality, painted the numbers, and fixed some bugs

--- script.js
+++ script.js
@@ -1,27 +1,112 @@
-const fieldWidth = 6
-const fieldHeight = 6
-const bombCount = 6;
+const fieldWidth = 12
+const fieldHeight = 12
+const bombCount = 30;
 
 const fieldGrid = document.querySelector("#field-grid")
 
 const fieldData = Array(fieldHeight).fill(Array()).map(() => Array(fieldWidth).fill(0))
 
+let clear_steps = 20;
+
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
-  }
+}
 
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+const explodeField = async () => {
+    for (let i = 0; i < fieldHeight; i++) {
+        for (let j = 0; j < fieldHeight; j++) {
+            if (fieldData[i][j] === 1) {
+                getHtmlSpot(i, j).innerText = "X";
+                await delay(10);
+            }
+        }
+    }
+}
+
+const getHtmlSpot = (row, col) => document.querySelector(`[row="${row}"][col="${col}"]`)
+
+const clearSpaces = (row, col) => {
+    if (--clear_steps < 0) {
+        return;
+    }
+
+    const current = getHtmlSpot(row, col);
+
+    if (current.getAttribute("status") !== "hidden") return;
+
+    current.setAttribute("status", "reveled")
+    
+    if (current.getAttribute("num") !== "0") {
+        current.innerText = current.getAttribute("num")
+        return;
+    }
+    
+    if (row - 1 >= 0) {
+        const up = getHtmlSpot(row-1, col)
+
+        if (up.getAttribute("status") === "hidden") {
+
+            if (up.getAttribute("num") !== null) {
+                clearSpaces(row-1, col)
+            }
+        }
+
+    }
+
+    if (row + 1 < fieldHeight) {
+        const down = getHtmlSpot(row+1, col)
+
+        if (down.getAttribute("status") === "hidden") {
+
+            if (down.getAttribute("num") !== null) {
+                clearSpaces(row+1, col)
+            }
+        }
+
+    }
+
+    if (col - 1 >= 0) {
+        const left = getHtmlSpot(row, col-1)
+
+        if (left.getAttribute("status") === "hidden") {
+
+            if (left.getAttribute("num") !== null) {
+                clearSpaces(row, col-1)
+            }
+        }
+
+    }
+
+    if (col + 1 < fieldWidth) {
+        const right = getHtmlSpot(row, col+1)
+
+        if (right.getAttribute("status") === "hidden") {
+
+            if (right.getAttribute("num") !== null) {
+                clearSpaces(row, col+1)
+            }
+        }
+
+    }
+}
   
 const spotClicked = (e) => {
     let row = e.getAttribute("row")
     let col = e.getAttribute("col")
 
     if (fieldData[row][col] == 1) {
-        alert("Booom!");
+        explodeField()
         return
     }
 
-    e.setAttribute("status", "reveled")
+    clearSpaces(row, col)
+    clear_steps = 20;
 }
 
 const startSpots = () => {
